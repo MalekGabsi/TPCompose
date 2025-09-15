21ebbcd7fb438c49873a3db76bc8efdf88687857
MalekGabsi <malekgabsi42@gmail.com>
2025-09-15 12:01:01 +0200

    changes

--- app/src/main/java/com/example/app0/MainActivity.kt
+++ app/src/main/java/com/example/app0/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import com.example.app0.ui.theme.App0Theme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -24,10 +25,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -77,25 +80,23 @@ fun EventScreen() {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(paddingValues)
+                    .padding(paddingValues),
+
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.event_image),
                     contentDescription = "Event Image",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(500.dp)
+                        .height(600.dp)
                         .padding(top = 20.dp)
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                Column(modifier = Modifier.padding(horizontal = 16.dp)) {
-                    Text(text = "24 Oct.", fontWeight = FontWeight.Bold)
+                Column(modifier = Modifier.padding(horizontal = 120.dp)) {
                     Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = "À la rencontre de nos talents")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = "Où : Ecole ingénieur ISIS")
+                    Text(text = "Où : Ecole ingénieur ISIS" )
                     Text(text = "Quand : 24 octobre")
                 }
 
@@ -105,8 +106,8 @@ fun EventScreen() {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
                     Button(onClick = { }, modifier = Modifier.weight(1f)) {
                         Text(text = "Inscription")
@@ -114,6 +115,7 @@ fun EventScreen() {
                     Button(
                         onClick = {},
                         modifier = Modifier.weight(1f)
+
                     ) {
                         Text(text = "Pas intéressé", color = Color.White)
                     }
